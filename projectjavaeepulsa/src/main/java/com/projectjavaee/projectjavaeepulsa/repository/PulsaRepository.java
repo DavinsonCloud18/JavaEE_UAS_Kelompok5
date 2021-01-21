801ab7586fb81cb90a62e77eff671f5607a8b9d2
Diana Octaviana <58798944+dianaoctaviana@users.noreply.github.com>
2021-01-21 08:58:31 +0700

    Add files via upload

---
+++ projectjavaeepulsa/src/main/java/com/projectjavaee/projectjavaeepulsa/repository/PulsaRepository.java
@@ -0,0 +1,9 @@
+package com.projectjavaee.projectjavaeepulsa.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.projectjavaee.projectjavaeepulsa.model.*;
+
+public interface PulsaRepository extends JpaRepository<Pulsa, Integer>{
+
+}
---
+++ projectjavaeepulsa/src/test/java/com/projectjavaee/projectjavaeepulsa/ProjectjavaeepulsaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projectjavaee.projectjavaeepulsa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectjavaeepulsaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
