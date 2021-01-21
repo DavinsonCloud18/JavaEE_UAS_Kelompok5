ff25e79bdbbaf551c5e35780d1e46eb6f7c61916
Diana Octaviana <58798944+dianaoctaviana@users.noreply.github.com>
2021-01-21 08:55:45 +0700

    Add files via upload
    PulsaBackEnd

---
+++ projectjavaeeruangan/src/main/java/com/projectjavaee/projectjavaeeruangan/repository/RuanganRepository.java
@@ -0,0 +1,7 @@
+package com.projectjavaee.projectjavaeeruangan.repository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.projectjavaee.projectjavaeeruangan.model.*;
+
+public interface RuanganRepository extends JpaRepository<Ruangan, Integer> {
+
+}
