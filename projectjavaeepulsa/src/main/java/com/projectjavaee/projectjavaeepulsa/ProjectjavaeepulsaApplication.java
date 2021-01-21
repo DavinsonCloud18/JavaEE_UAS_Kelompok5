df97743dca8ae3cfdc48e6ea25198552290289c7
Diana Octaviana <58798944+dianaoctaviana@users.noreply.github.com>
2021-01-22 00:25:56 +0700

    pulsa, pembayaran pulsa dan penjualan pulsa

---
+++ projectjavaeepulsa/src/main/java/com/projectjavaee/projectjavaeepulsa/ProjectjavaeepulsaApplication.java
@@ -0,0 +1,13 @@
+package com.projectjavaee.projectjavaeepulsa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectjavaeepulsaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectjavaeepulsaApplication.class, args);
+	}
+
+}
